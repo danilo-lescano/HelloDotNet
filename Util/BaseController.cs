4f8aec2dcf035541c733210b48ca984f5526169d
danilocaverna <nq3i4fsx@hotmail.com>
2019-01-08 15:29:34 -0300

    explorando opções de nova sessão

---
+++ Util/BaseController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
+using Microsoft.AspNetCore.Mvc;
+using tacertoforms_dotnet.Models;
+using TaCertoForms.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace Util{
+    public class BaseController : Controller{
+        
+    }
+}
