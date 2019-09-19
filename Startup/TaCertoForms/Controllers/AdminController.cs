6ea027e4abb181d866302a5dba3037870d9d01bf
danilocaverna <nq3i4fsx@hotmail.com>
2019-09-19 09:16:46 -0400

    ++

--- Startup/TaCertoForms/Controllers/AdminController.cs
+++ Startup/TaCertoForms/Controllers/AdminController.cs
@@ -10,9 +10,30 @@ using TaCertoForms.Util.Coercion;
 
 namespace TaCertoForms.Controllers{
     public class AdminController : SessionController{
+        public IActionResult Index(){
+            Start();
+            if(isUsuarioLogado()){
+                //redirecionar para admin, aluno, professor dependendo do perfil
+                return RedirectToAction("Index", ((Perfil)Session["Perfil"]).Nome);
+            }
+            else{
+                //n tem login
+                return View();
+            }
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error(){
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        
+        private void Start(){
+            GetSession();
+        }
+        private bool isUsuarioLogado(){
+            if(Session["IsLogged"] != null && Coercion.ToBool(Session["IsLogged"]))
+                return true;
+            return false;
         }
     }
 }
