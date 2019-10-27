6ca2bf3db294bedd9158b1e8a96b196c4c8e184d
danilocaverna <nq3i4fsx@hotmail.com>
2019-10-27 14:00:57 -0300

    factory wip

---
+++ Startup/tacertoforms .net 4/tacertoforms/Factory/MyModelFactory/FactoryCollectionMatriz.cs
@@ -0,0 +1,12 @@
+namespace TaCertoForms.Factory{
+    public class FactoryCollectionMatriz : ICollection{
+        private Context db = new Context();
+
+        public Pessoa FindPessoa(int? id){
+
+        }
+        public List<Pessoa> PessoasList(){
+
+        }
+    }
+}
---
+++ Startup/tacertoforms .net 4/tacertoforms/Factory/MyModelFactory/Interface Creators/ICollection.cs
@@ -0,0 +1,1 @@
+public interface ICollection : IPessoa, IAtividade{}
