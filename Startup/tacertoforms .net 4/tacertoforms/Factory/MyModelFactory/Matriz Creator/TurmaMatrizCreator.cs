1715534779508437c8cb3c7e9737a5e9c9123c8c
danilocaverna <nq3i4fsx@hotmail.com>
2019-10-30 22:55:57 -0300

    ++

--- Startup/tacertoforms .net 4/tacertoforms/Factory/MyModelFactory/Matriz Creator/TurmaMatrizCreator.cs
+++ Startup/tacertoforms .net 4/tacertoforms/Factory/MyModelFactory/Matriz Creator/TurmaMatrizCreator.cs
@@ -3,20 +3,18 @@ using System.Collections.Generic;
 using TaCertoForms.Models;
 using TaCertoForms.Contexts;
 
-namespace TaCertoForms.Factory
-{
-    public class TurmaMatrizCreator : BaseCreator, IFactoryTurma
-    {
+namespace TaCertoForms.Factory{
+    public class TurmaMatrizCreator : BaseCreator, IFactoryTurma{
+        
         public TurmaMatrizCreator(int IdMatriz, int IdPessoa) : base(IdMatriz, IdPessoa) { }
-        public Turma CreateTurma(Turma turma)
-        {
+        
+        public Turma CreateTurma(Turma turma){
             Context db = new Context();
             Pessoa pessoa = db.Pessoa.Find(IdPessoa);
             Instituicao instituicao = db.Instituicao.Find(turma.IdInstituicao);
             if (pessoa == null || instituicao == null) return null; //Caso seja uma requisição absurda
             
-            if (instituicao.IdInstituicao == IdMatriz || (instituicao.IdMatriz != null && instituicao.IdMatriz == IdMatriz))
-            {   
+            if (instituicao.IdInstituicao == IdMatriz || (instituicao.IdMatriz != null && instituicao.IdMatriz == IdMatriz)){   
                 //Salvando a turma                
                 db.Turma.Add(turma);
                 db.SaveChanges();
@@ -25,21 +23,18 @@ namespace TaCertoForms.Factory
             }
             return null; 
         }
-        public bool DeleteTurma(int? id)
-        {            
+        public bool DeleteTurma(int? id){            
             throw new System.NotImplementedException();
         }
 
-        public Turma EditTurma(Turma turma)
-        {
+        public Turma EditTurma(Turma turma){
             //TODO Como será se após vinculado turma com aluno e disciplina, o usuário mudasse a turma?
             Context db = new Context();
             Pessoa pessoa = db.Pessoa.Find(IdPessoa);
             Instituicao instituicao = db.Instituicao.Find(turma.IdInstituicao);
             if (pessoa == null || instituicao == null) return null;
 
-            if (instituicao.IdInstituicao == IdMatriz || (instituicao.IdMatriz != null && instituicao.IdMatriz == IdMatriz))
-            {   
+            if (instituicao.IdInstituicao == IdMatriz || (instituicao.IdMatriz != null && instituicao.IdMatriz == IdMatriz)){   
                 db.Entry(turma).State = System.Data.Entity.EntityState.Modified;
                 db.SaveChanges();                
                 db.Dispose();
@@ -48,8 +43,7 @@ namespace TaCertoForms.Factory
             return null; //Caso deu erro ao salvar 
         }
 
-        public Turma FindTurma(int? id)
-        {
+        public Turma FindTurma(int? id){
             if (id == null) return null;
 
             Context db = new Context();
@@ -63,8 +57,7 @@ namespace TaCertoForms.Factory
             return null;
         }
 
-        public List<Turma> TurmaList()
-        {
+        public List<Turma> TurmaList(){
             Context db = new Context();
             Pessoa pessoa = db.Pessoa.Find(IdPessoa);            
             
@@ -74,8 +67,7 @@ namespace TaCertoForms.Factory
             idAuxList = new List<int>();
             foreach (var i in instituicaoList) idAuxList.Add(i.IdInstituicao);
             
-            if (idAuxList.Contains(IdMatriz))
-            {                
+            if (idAuxList.Contains(IdMatriz)){                
                 List<Turma> turmas = db.Turma.Where(a => idAuxList.Contains(a.IdInstituicao)).ToList();
                 return turmas;
             }            
