a2b95468520804531e4c559be1ee2551e4eab959
danilocaverna <nq3i4fsx@hotmail.com>
2019-11-04 23:24:07 -0300

    TurmaAlunoProfessorCreator FindTurmaAluno & TurmaAlunoList

--- Startup/tacertoforms .net 4/tacertoforms/Factory/MyModelFactory/Professor Creator/TurmaAlunoProfessorCreator.cs
+++ Startup/tacertoforms .net 4/tacertoforms/Factory/MyModelFactory/Professor Creator/TurmaAlunoProfessorCreator.cs
@@ -9,11 +9,42 @@ namespace TaCertoForms.Factory{
         public TurmaAlunoProfessorCreator(int IdMatriz, int IdPessoa) : base(IdMatriz, IdPessoa) { }
 
         public TurmaAluno FindTurmaAluno(int? id){
-            throw new System.NotImplementedException();
+            if(id == null) return null;
+            Context db = new Context();
+            List<int> idAuxList;
+
+            List<TurmaDisciplinaAutor> turmaDisciplinaAutorList = db.TurmaDisciplinaAutor.Where(tda => tda.IdAutor == IdPessoa).ToList();
+            if(turmaDisciplinaAutorList == null || turmaDisciplinaAutorList.Count == 0) return null;
+            idAuxList = new List<int>();
+            foreach(var tda in turmaDisciplinaAutorList) idAuxList.Add(tda.IdDisciplinaTurma);
+
+            List<DisciplinaTurma> disciplinaTurmaList = db.DisciplinaTurma.Where(dt => idAuxList.Contains(dt.IdDisciplinaTurma)).ToList();
+            if(disciplinaTurmaList == null || disciplinaTurmaList.Count == 0) return null;
+            idAuxList = new List<int>();
+            foreach(var dt in disciplinaTurmaList) idAuxList.Add(dt.IdTurma);
+
+            Turma turma = db.Turma.Where(t => t.IdTurma == id && idAuxList.Contains(t.IdTurma)).FirstOrDefault();
+
+            return turma;
         }
 
         public List<TurmaAluno> TurmaAlunoList(){
-            throw new System.NotImplementedException();
+            Context db = new Context();
+            List<int> idAuxList;
+
+            List<TurmaDisciplinaAutor> turmaDisciplinaAutorList = db.TurmaDisciplinaAutor.Where(tda => tda.IdAutor == IdPessoa).ToList();
+            if(turmaDisciplinaAutorList == null || turmaDisciplinaAutorList.Count == 0) return null;
+            idAuxList = new List<int>();
+            foreach(var tda in turmaDisciplinaAutorList) idAuxList.Add(tda.IdDisciplinaTurma);
+
+            List<DisciplinaTurma> disciplinaTurmaList = db.DisciplinaTurma.Where(dt => idAuxList.Contains(dt.IdDisciplinaTurma)).ToList();
+            if(disciplinaTurmaList == null || disciplinaTurmaList.Count == 0) return null;
+            idAuxList = new List<int>();
+            foreach(var dt in disciplinaTurmaList) idAuxList.Add(dt.IdTurma);
+
+            List<Turma> turmaList = db.Turma.Where(t => && idAuxList.Contains(t.IdTurma)).ToList();
+
+            return turmaList;
         }
 
         public TurmaAluno CreateTurmaAluno(TurmaAluno turmaAluno){
